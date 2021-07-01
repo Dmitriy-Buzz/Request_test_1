7e5d6de42b8c3b3bcfd66c8a396dc3d5b6bfd799
Dmitriy Kukushkin <mrdikukushkin@yandex.ru>
2021-07-01 12:36:57 +0500

    Reqiest_1_v2

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
-from pprint import pprint
-
 import requests
+import operator
 
 TOKEN = "2619421814940190"
 
-
+hero_lict= []
 def test_request_h():
+    url = "https://superheroapi.com/api/2619421814940190/search/Hulk"
+    headers = {"Authorization": TOKEN}
+    response = requests.get(url=url, headers=headers, timeout=5)
+    hulk = response.json()
+    name = hulk["results-for"]
+    intel = int(hulk["results"][0]["powerstats"]["intelligence"])
+    hero_dict = {"Имя":name, "Интелект": intel}
+    hero_lict.append(hero_dict)
+
+def test_request_t():
     url = "https://superheroapi.com/api/2619421814940190/search/Thanos"
     headers = {"Authorization": TOKEN}
     response = requests.get(url=url, headers=headers, timeout=5)
-    pprint(response.json())
+    thanos = response.json()
+    name = thanos["results-for"]
+    intel = int(thanos["results"][0]["powerstats"]["intelligence"])
+    hero_dict = {"Имя":name, "Интелект": intel}
+    hero_lict.append(hero_dict)
+def test_request_ca():
+    url = "https://superheroapi.com/api/2619421814940190/search/Captain America"
+    headers = {"Authorization": TOKEN}
+    response = requests.get(url=url, headers=headers, timeout=5)
+    cap = response.json()
+    name = cap["results-for"]
+    intel = int(cap["results"][0]["powerstats"]["intelligence"])
+    hero_dict = {"Имя":name, "Интелект": intel}
+    hero_lict.append(hero_dict)
 
+def sort_():
+    hero_lict.sort(key=operator.itemgetter("Интелект"))
+    print(f"Самый умный герой:{hero_lict[-1]}")
 
 test_request_h()
+test_request_t()
+test_request_ca()
+sort_()
